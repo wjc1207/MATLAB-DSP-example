b8fd32acf7fdaf265e2f1a76cbfec91d7e2816d3
wjc74751 <85719815+wjc74751@users.noreply.github.com>
2023-03-28 19:39:10 +0800

    Add files via upload

--- fft_example.m
+++ fft_example.m
@@ -8,4 +8,7 @@ N = T / deltaT
 x = sin(2*pi*100*t) % signal x(t)
 y = fft(x) % y(t) = F{x(t)}
 y = fftshift(y)
-plot(-fs/2:fs/N:fs/2,abs(y))
+plot(-fs/2:fs/N:fs/2,abs(y)/N)
+title("FFT Demo")
+ylabel("X(f)")
+xlabel("x(t)")
