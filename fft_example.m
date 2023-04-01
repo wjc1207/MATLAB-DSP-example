2370f007f73a30de223665b0b786303bd214506d
wjc74751 <85719815+wjc74751@users.noreply.github.com>
2023-04-01 23:04:40 +0800

    Add files via upload

--- fft_example.m
+++ fft_example.m
@@ -5,7 +5,7 @@ fs = 1 / deltaT
 N = T / deltaT
 
 
-x = sin(2*pi*100*t) % signal x(t)
+x = 5 * sin(2*pi*100*t) + 2 * sin(2*pi*40*t) % signal x(t)
 y = fft(x) % y(t) = F{x(t)}
 y = fftshift(y)
 plot(-fs/2:fs/N:fs/2,abs(y)/N)
